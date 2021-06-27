c64259e3699ffcc51f45214611c11651f400a765
mirac <mirac@gelal.com>
2021-06-27 15:24:31 +0300

    first_commit

--- TodoApi/Entity/Product.cs
+++ TodoApi/Entity/Product.cs
@@ -3,7 +3,7 @@ using TodoApi.Base;
 namespace TodoApi.Entity
 {
     public class Product : BaseEntity
-    {
+    { 
         public string Name { get; set; }
         public int CategoryId { get; set; }
         public decimal Price { get; set; }
